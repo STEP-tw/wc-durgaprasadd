c0d60fdf1b5f9170146eff0fe0e9572ab25e3f0f
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-12-26 15:26:44 +0530

    Added test case for new line at the end.

--- test/libTest.js
+++ test/libTest.js
@@ -3,7 +3,8 @@ const { wc } = require('../src/lib.js');
 
 const contents = {
   file: 'a',
-  file1: 'a b\nc d'
+  file1: 'a\n',
+  file2: 'a b\nc d'
 };
 
 const reader = fileName => contents[fileName];
@@ -12,6 +13,11 @@ describe('wc', () => {
   it('should return wordCount, lineCount and characterCount', () => {
     let actual = wc(['file'], reader);
     let expected = '\t1\t1\t1 file';
+    assert.deepEqual(actual, expected);
+  });
+  it('should not count extra new line character at last', () => {
+    let actual = wc(['file1'], reader);
+    let expected = '\t1\t1\t2 file1';
     assert.deepEqual(actual, expected);
   });
 });
